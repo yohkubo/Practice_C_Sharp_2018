d57fb71a7b7dcef769cab174c294daaba345a7a5
Yohei <yo_kubocchi@hotmail.com>
2018-05-22 12:25:08 +0900

    Practice

---
+++ CS_P025_Strings/CS_P025_Strings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P025_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jesse";
+            string quote = "The man is \"flash\". \nI am studing at home today.\n\tYou gotta come here.";
+            string fileName = @"C:\Users\Jesse";
+
+            //bool trueOrFalse = name.Contains("e");
+            //trueOrFalse = name.EndsWith("k");
+            //Console.WriteLine(trueOrFalse);
+            //int nameLength = name.Length;
+            //Console.WriteLine(nameLength);
+
+            //name = name.ToUpper();
+            ////Console.WriteLine("Hello, " + name + ". " + quote);
+            //Console.WriteLine(name);
+            //name = name.ToLower();
+            //Console.WriteLine(name);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Jesse\n");
+            sb.Append("Yohei");
+
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ CS_P026_Exercise_Strings/CS_P026_Exercise_Strings/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P026_Exercise_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your name by separating first name, middle name, and last name.\nFirst name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Middle name:");
+            string middleName = Console.ReadLine();
+            Console.WriteLine("Last name:");
+            string lastName = Console.ReadLine();
+            
+            string fullName = firstName + " " + middleName + " " + lastName;
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(fullName.ToUpper());
+
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Hi, I am joining this program from Japan at the morment.");
+            sb.Append("I am studing hard lately and I have to finish this within 4 weeks.\n");
+            sb.Append("After this program, I hope I will become a web developer and go to the USA for work.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
