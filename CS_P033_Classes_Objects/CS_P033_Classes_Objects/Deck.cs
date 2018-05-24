f9fbc2fb0a12794999b0291d62caf3bb9c0d16d0
Yohei <yo_kubocchi@hotmail.com>
2018-05-24 15:44:12 +0900

    Practice _ Class and Object

---
+++ CS_P033_Classes_Objects/CS_P033_Classes_Objects/Deck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P033_Classes_Objects
+{
+    class Deck
+    {
+        public Deck()
+        {
+            //Cards = new List<Card>();
+
+            List<string> Suits = new List<string>() { "Clubs", "Spades", "Hearts", "Diamonds" };
+            List<string> Faces = new List<string>()
+            { "Two","Three","Four","Five","Six","Seven",
+                "Eight","Nine","Ten","Jack","Queen","King","Ace"};
+            
+            foreach(string suit in Suits)
+            {
+                foreach (string face in Faces)
+                {
+                    Card cardOne = new Card();
+                    cardOne.Suit = suit;
+                    cardOne.Face = face;
+                    Cards.Add(cardOne);
+                }
+            }
+
+        }
+            
+        public List<Card> Cards = new List<Card>();
+    }
+}
---
+++ CS_P033_Classes_Objects/CS_P033_Classes_Objects/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P033_Classes_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Card cardOne = new Card();
+            //cardOne.Face = "Queen";
+            //cardOne.Suit = "Spades";
+
+            Deck deck = new Deck();
+
+            for(int i = 0; i < deck.Cards.Count; i++)
+            {
+            Console.WriteLine(deck.Cards[i].Face + " of " + deck.Cards[i].Suit);
+            }
+            Console.WriteLine("Amount of cards: " + deck.Cards.Count);
+            Console.ReadLine();
+        }
+    }
+}
