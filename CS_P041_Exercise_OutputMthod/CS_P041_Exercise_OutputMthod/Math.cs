b2a63e574cb980652fe80a68a8f8acf47a09abbe
Yohei <yo_kubocchi@hotmail.com>
2018-05-28 11:17:49 +0900

    Practice

---
+++ CS_P041_Exercise_OutputMthod/CS_P041_Exercise_OutputMthod/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P041_Exercise_OutputMthod
+{
+    public static class Math //Exercise 6
+    {
+        // Exercise 1, 4
+        public void Divide(int num1, out int result)
+        {
+            result = num1 / 2;
+        }
+
+        //Eercise 5
+        public int Divide(int num1)
+        {
+            int result = num1 / 2;
+            return result;
+        }
+
+        public void Divide(string data1)
+        {
+            Console.WriteLine("You entered '" + data1 + "', not a whole number.");
+        }
+    }
+}
---
+++ CS_P041_Exercise_OutputMthod/CS_P041_Exercise_OutputMthod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P041_Exercise_OutputMthod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math(); // Exercise 2
+            Console.WriteLine("Enter a whole number: "); // Exercise 3
+            string input1 = Console.ReadLine();
+            int num = 0;
+            int result = 0;
+
+            bool isInt = int.TryParse(input1, out num);
+            if (!isInt)
+            {
+                math.Divide(input1);
+            }
+            else if (isInt)
+            {
+                //Console.WriteLine("Using retuning int 'Divide' method: " + math.Divide(num));
+
+                math.Divide(num, out result);
+                Console.WriteLine("Using void 'Divide' method: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Something is wrong.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
