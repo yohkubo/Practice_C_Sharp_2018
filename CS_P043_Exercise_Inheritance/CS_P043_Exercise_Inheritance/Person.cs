7dadd7c8f034be99202e2591414a39d8b4e50deb
Yohei <yo_kubocchi@hotmail.com>
2018-05-28 18:18:13 +0900

    practice

---
+++ CS_P043_Exercise_Inheritance/CS_P043_Exercise_Inheritance/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P043_Exercise_Inheritance
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            string fullName = LastName + ", " + FirstName;
+            Console.WriteLine("Name: " + fullName);
+        }
+    }
+}
---
+++ CS_P043_Exercise_Inheritance/CS_P043_Exercise_Inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P043_Exercise_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
