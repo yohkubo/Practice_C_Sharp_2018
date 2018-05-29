5a7d4b8b8acd65c0cf5a4f3e69a95646b7b51fb0
Yohei <yo_kubocchi@hotmail.com>
2018-05-29 20:19:01 +0900

    P053_OperatorOverride

--- CS_P047_Exercise_AbstractClass/CS_P047_Exercise_AbstractClass/Employee.cs
+++ CS_P047_Exercise_AbstractClass/CS_P047_Exercise_AbstractClass/Employee.cs
@@ -8,8 +8,9 @@ namespace CS_P047_Exercise_AbstractClass
 {
     class Employee : Person, IQuittable
     {
-
+        public int EmployeeId { get; set; }
         public bool isActiveEmployee { get; set; }
+        
 
         public override void SayName(string lastName, string firstName)
         {
@@ -20,5 +21,26 @@ namespace CS_P047_Exercise_AbstractClass
         {
             Console.WriteLine("Quittable.");
         }
+
+        // operator== 
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            return (emp1.EmployeeId == emp2.EmployeeId);
+        } 
+        public static bool operator!=(Employee emp1, Employee emp2)
+        {
+            Console.WriteLine(emp1.firstName + "'ID and " + emp2.firstName + "'s ID are different. No problem.");
+
+            return (emp1.EmployeeId != emp2.EmployeeId);
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
     }
 }
--- CS_P047_Exercise_AbstractClass/CS_P047_Exercise_AbstractClass/Program.cs
+++ CS_P047_Exercise_AbstractClass/CS_P047_Exercise_AbstractClass/Program.cs
@@ -13,10 +13,18 @@ namespace CS_P047_Exercise_AbstractClass
             //Person person = new Employee() { firstName = "Sample", lastName = "Student" };
             //person.SayName(person.lastName, person.firstName);
 
-            IQuittable quittable = new Employee() { firstName = "Sample", lastName = "Employee" };
-            
-            quittable.Quit();
-            
+            //IQuittable quittable = new Employee() { firstName = "Sample", lastName = "Employee" };
+            //quittable.Quit();
+
+            List<Employee> employees = new List<Employee>();
+            Employee employee1 = new Employee() { firstName = "Yohei", lastName = "Kubo", EmployeeId = 0 };
+            Employee employee2 = new Employee() { firstName = "Kensuke", lastName = "Kubo", EmployeeId = 1 };
+            Employee employee3 = new Employee() { firstName = "Ryusuke", lastName = "Kubo", EmployeeId = 0 };
+
+            Console.WriteLine(employee1 == employee2);
+            Console.WriteLine(employee2 == employee3);
+            Console.WriteLine(employee3 == employee1);
+
             Console.ReadLine();
         }
     }
