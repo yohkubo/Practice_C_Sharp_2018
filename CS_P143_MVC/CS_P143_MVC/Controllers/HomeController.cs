82a633f4f7d45f2ccb41621212ba13e49d8dc9c4
Yohei <yo_kubocchi@hotmail.com>
2018-06-25 10:46:25 +0900

    practice

---
+++ CS_P143_MVC/CS_P143_MVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using CS_P143_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CS_P143_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Jesse";
+            user.LastName = "Johnson";
+            user.Age = 30;
+
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            throw new Exception("Invalid page.mannnnnnnnn!!!!");
+
+            //return View();
+        }
+
+        public ActionResult Contact(string id="Kent")
+        {
+            ViewBag.Message = "Today's lucky number is:" + id;
+
+            return View();
+        }
+    }
+}
